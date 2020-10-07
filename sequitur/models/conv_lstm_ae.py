2736740ac301b98b35e0f6a4fb728be054cb908c
shobrook <shobrookj@gmail.com>
2020-10-07 05:31:41 -0400

    Removed in_channels parameter

--- sequitur/models/conv_lstm_ae.py
+++ sequitur/models/conv_lstm_ae.py
@@ -12,20 +12,19 @@ from .conv_ae import CONV_AE
 
 
 class CONV_LSTM_AE(nn.Module):
-    def __init__(self, input_dims, encoding_dim, in_channels=1, kernel=None,
-                 stride=None, h_conv_channels=[], h_lstm_channels=[]):
+    def __init__(self, input_dims, encoding_dim, kernel, stride=1,
+                 h_conv_channels=[1], h_lstm_channels=[]):
         super(CONV_LSTM_AE, self).__init__()
 
         self.input_dims = input_dims
-        self.conv_enc_dim = sum(input_dims) * in_channels
+        self.conv_enc_dim = sum(input_dims)
 
         self.conv_ae = CONV_AE(
             input_dims,
             self.conv_enc_dim,
-            in_channels,
-            h_conv_channels,
             kernel,
-            stride
+            stride,
+            h_channels=h_conv_channels
         )
         self.lstm_ae = LSTM_AE(
             self.conv_enc_dim,
@@ -36,26 +35,22 @@ class CONV_LSTM_AE(nn.Module):
     def encoder(self, x):
         n_elements, encodings = x.shape[0], []
         for i in range(n_elements):
-            element = x[i].reshape((
-                1,
-                1,
-                *x[i].shape
-            ))
+            element = x[i].unsqueeze(0).unsqueeze(0)
             encodings.append(self.conv_ae.encoder(element))
 
         return self.lstm_ae.encoder(torch.stack(encodings))
 
     def decoder(self, x, seq_len):
-        encodings = self.lstm_ae.decoder(torch.squeeze(x), seq_len)
+        encodings = self.lstm_ae.decoder(x, seq_len)
         decodings = []
         for i in range(seq_len):
-            decodings.append(self.conv_ae.decoder(encodings[i]))
+            decodings.append(torch.squeeze(self.conv_ae.decoder(encodings[i])))
 
         return torch.stack(decodings)
 
     def forward(self, x):
         seq_len = x.shape[0]
-        x = self.encode(x)
-        x = self.decode(x, seq_len)
+        x = self.encoder(x)
+        x = self.decoder(x, seq_len)
 
         return x
