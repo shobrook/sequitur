2766e2ddc7db31337d2c1511ac8bb734da5e5bf6
Quoding <quoding@protonmail.ch>
2021-08-02 09:17:15 -0400

    Bug fixes and enhancement in quick_train Fix bug where get_encodings would not have the right device (add get_device()) Add gradient clipping to quick_train with clip_value argument

--- sequitur/quick_train.py
+++ sequitur/quick_train.py
@@ -11,6 +11,10 @@ from torch.nn import MSELoss
 ###########
 
 
+def get_device():
+    return torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
 def instantiate_model(model, train_set, encoding_dim, **kwargs):
     if model.__name__ in ("LINEAR_AE", "LSTM_AE"):
         return model(train_set[-1].shape[-1], encoding_dim, **kwargs)
@@ -21,9 +25,11 @@ def instantiate_model(model, train_set, encoding_dim, **kwargs):
             return model(train_set[-1].shape[-3:], encoding_dim, **kwargs)
 
 
-def train_model(model, train_set, verbose, lr, epochs, denoise, device=None):
+def train_model(
+    model, train_set, verbose, lr, epochs, denoise, clip_value, device=None
+):
     if device is None:
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        device = get_device()
     model.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     criterion = MSELoss(reduction="sum")
@@ -49,6 +55,11 @@ def train_model(model, train_set, verbose, lr, epochs, denoise, device=None):
 
             # Backward pass
             loss.backward()
+
+            # Gradient clipping on norm
+            if clip_value is not None:
+                torch.nn.utils.clip_grad_norm_(model.parameters(), clip_value)
+
             optimizer.step()
 
             losses.append(loss.item())
@@ -62,9 +73,11 @@ def train_model(model, train_set, verbose, lr, epochs, denoise, device=None):
     return mean_losses
 
 
-def get_encodings(model, train_set):
+def get_encodings(model, train_set, device=None):
+    if device is None:
+        device = get_device()
     model.eval()
-    encodings = [model.encoder(x) for x in train_set]
+    encodings = [model.encoder(x.to(device)) for x in train_set]
     return encodings
 
 
@@ -80,11 +93,12 @@ def quick_train(
     verbose=False,
     lr=1e-3,
     epochs=50,
+    clip_value=1,
     denoise=False,
     **kwargs,
 ):
     model = instantiate_model(model, train_set, encoding_dim, **kwargs)
-    losses = train_model(model, train_set, verbose, lr, epochs, denoise)
+    losses = train_model(model, train_set, verbose, lr, epochs, denoise, clip_value)
     encodings = get_encodings(model, train_set)
 
     return model.encoder, model.decoder, encodings, losses
